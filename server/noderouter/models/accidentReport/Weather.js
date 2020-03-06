ae1d9e33ad3218c8cfdfbbe481cfe881d410de52
Aaron Gonzalez <AmG3535677>
2020-03-06 01:45:57 -0500

    Another one

--- server/noderouter/models/accidentReport/Weather.js
+++ server/noderouter/models/accidentReport/Weather.js
@@ -1,3 +1,3 @@
-const Weather = ["Hail", "Snow", "Rain", "Fog","Wind", "Clear"]; /*Weather enum*/
+const Weather = ["Hail", "Snow", "Rain", "Fog", "Wind", "Clear"]; /*Weather enum*/
 /*const Weather = ["inset a list of weather to be determined here"];*/
 module.exports = Weather;
