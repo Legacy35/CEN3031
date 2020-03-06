3b67d600098b654920a768c65391c197b9a88480
Aaron Gonzalez <AmG3535677>
2020-03-06 01:02:33 -0500

    Added Accident Report Route MalInformation Check

--- server/noderouter/models/accidentReport/Weather.js
+++ server/noderouter/models/accidentReport/Weather.js
@@ -1,2 +1,2 @@
-const Weather = ["Sunny", "Raining", "Foggy"]; /*Weather enum*/
+export const Weather = ["Sunny", "Raining", "Foggy"]; /*Weather enum*/
 /*const Weather = ["inset a list of weather to be determined here"];*/
