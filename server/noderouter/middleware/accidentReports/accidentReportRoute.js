04a931f8dbc07b8883b49df9c9d38c5f43b111de
Cameron Whtie <cameron.white@ufl.edu>
2020-03-06 03:02:05 -0500

    Fixed v 0.0.3

--- server/noderouter/middleware/accidentReports/accidentReportRoute.js
+++ server/noderouter/middleware/accidentReports/accidentReportRoute.js
@@ -1,73 +1,80 @@
 /**
  * File is currently a work in progress, do not modify
  */
-const WeatherEnum = require('./../../models/accidentReport/Weather.js')
+const axios = require('axios');
+
 const AccidentReport = require('./../../models/accidentReport/AccidentReport.js');
-const State =['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+const State = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+const config = require('../../config.js')
+
 const accidentReportPost = (req, res) => {
-  axios.get('/apis/authenticate/whois.php?token=' + req.cookies.token).then(
-                        (res) => {
-                        if(res.data.error){
-                              res.status(404);
-                              res.send({error: res.data.error + " User may not exist"});
-                              return;}
-                        if(!res.data.isAdmin){
-                              res.status(401);
-                              res.send({error: "Unauthorized"});
-                              return;
-                        }
-                      }
-                    ).catch(
-                            (err) => {
-                                console.log(err);
-                            }
-                        );
+     axios.get(config.authServer + 'whois.php?token=' + req.cookies.token).then(
+        (resp) => {
+            if (resp.data.error) {
+                res.send({ error: + "User may not exist" });
+                return;
+            }
+            if (!resp.data.admin) {
+                res.send({ error: "Unauthorized" });
+                return;
+            } else {
 
-  if (!req.body.cityName || !req.body.state || !req.body.date || !req.body.weather) {
-    res.status(400).send({
-      error: "Malformed request",
-    });
-    return;
-  }else if(req.body.date<0 ||req.body.date>new Date()){
-    res.status(400).send({
-      error: "Malformed request, Date Incorrect",
-    });
-    return;
-  }else if(State.indexOf(req.body.state)==-1){
-    res.status(400).send({
-      error: "Malformed request, State Incorrect",
-    });
+            }
+        }
+    ).catch(
+        (err) => {
+            if (err) {
+                res.send({ error: "Error" });
+                console.log(err);
+                return;
+            }
+        }
+    );
 
-    return;
-  }else if(cityName.length>2 && cityName.length<46){
-    res.status(400).send({
-      error: "Malformed request, City Name Incorrect",
-    });
-    return;
-  }else if((req.body.weather.size>0 && req.body.weather.size < exports.Weather.size()-1)){
-    res.status(400).send({
-      error: "Malformed request, Weather Incorrect",
-    });
-    return;
-  }
-
-  const db = DATABASES.accidentReports;
-  const model = db.model('AccidentReport', AccidentReport, 'accidentReports');
-  const doc = new model({
-    cityName: req.body.cityName,
-    state: req.body.state,
-    date: req.body.date,
-    weather: req.body.weather
-  });
-  doc.save((err, doc) => {
-    if (err) {
-      console.log(err);
-      res.send('err');
-      return;
-    } else {
-      console.log('Saved');
+    if (!req.body.cityName || !req.body.state || !req.body.date || !req.body.weather) {
+        res.send({
+            error: "Malformed request",
+        });
+        return;
+    } else if (req.body.date < 0 || req.body.date > new Date()) {
+        res.send({
+            error: "Malformed request, Date Incorrect",
+        });
+        return;
+    } else if (State.indexOf(req.body.state) == -1) {
+        res.send({
+            error: "Malformed request, State Incorrect",
+        });
+        return;
+    } else if (req.body.cityName.length < 2 || req.body.cityName.length > 46) {
+        res.send({
+            error: "Malformed request, City Name Incorrect",
+        });
+        return;
+    } else if ((req.body.weather.size > 0 && req.body.weather.size < exports.Weather.size() - 1)) {
+        res.send({
+            error: "Malformed request, Weather Incorrect",
+        });
+        return;
     }
-  });
+
+    const db = DATABASES.accidentReports;
+    const model = db.model('AccidentReport', AccidentReport, 'accidentReports');
+    const doc = new model({
+        cityName: req.body.cityName,
+        state: req.body.state,
+        date: req.body.date,
+        weather: req.body.weather
+    });
+    doc.save((err, doc) => {
+        if (err) {
+            console.log(err);
+            res.send('err');
+            return;
+        } else {
+            res.send('OK');
+        }
+    });
 
 }
 
