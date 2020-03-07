8898054780edb48d4ea96fb49641eeb988ab2a17
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-03-07 16:03:50 -0500

    Create QuizTab.js

---
+++ client/src/components/QuizTab.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
+const QuizTab = (props) => {
+
+        return (
+            <div className="tile view form">
+                <h2>Take a Driving Safety Quiz</h2>
+                <p><b>Quiestion 1:</b></p>
+                <p>What is a speed limit?</p>
+                <p>whaaa</p>
+            </div>
+        );
+}
+
+export default QuizTab;
