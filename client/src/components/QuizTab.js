0546a638c8bdba514fcd2a72d8f49de72b7c64b7
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-03-07 16:07:22 -0500

    Update Quiz.js

--- client/src/components/QuizTab.js
+++ client/src/components/QuizTab.js
@@ -9,8 +9,16 @@ const QuizTab = (props) => {
             <div className="tile view form">
                 <h2>Take a Driving Safety Quiz</h2>
                 <p><b>Quiestion 1:</b></p>
-                <p>What is a speed limit?</p>
-                <p>whaaa</p>
+                <p>Who is a speed limit?</p>
+                <p><b>Quiestion 2:</b></p>
+                <p>What is the speed limit?</p>
+                <p><b>Quiestion 3:</b></p>
+                <p>Where is your speed limit?</p>
+                <p><b>Quiestion 4:</b></p>
+                <p>When is our speed limit?</p>
+                <p><b>Quiestion 5:</b></p>
+                <p>Why is my speed limit?</p>
+                
             </div>
         );
 }
