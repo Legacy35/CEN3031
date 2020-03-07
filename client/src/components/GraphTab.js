5fcff7ad17b8ba9a7d94967001edeb98bb3db076
Garrick Sewsankar <gsewsankar@ufl.edu>
2020-03-07 16:47:06 -0500

    chart.js installation

---
+++ client/src/components/GraphTab.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
+const GraphTab = (props) => {
+
+        return (
+            <p>hello</p>
+        );
+}
+
+export default GraphTab;
