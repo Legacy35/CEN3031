f3d1b66caec6edba2222218d708fde9be9f7c94b
Cameron Whtie <cameron.white@ufl.edu>
2020-04-09 00:37:01 -0400

    Fixed

--- client/src/SessionManager.js
+++ client/src/SessionManager.js
@@ -11,7 +11,6 @@ export const login = (email, password, views, setViews, userData, setUserData) =
 
     axios.get("/apis/authenticate/authenticate.php?email=" + email + "&password=" + password).then(
         (res) => {
-          console.log(res.data);
             if (res.data.error) {
                 alert(res.data.error)
             } else if (res.data.token) {
@@ -81,9 +80,6 @@ export const loadProfile = async (views, setViews, userData, setUserData, select
                 }
             } else if (res.data.error) {
                 logout(views, setViews, setUserData);
-            }else{
-              console.log(res.data);
-              alert("You stupid");
             }
         }
     ).catch(
