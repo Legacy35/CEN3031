db2cd44a241b0fff134fb97ff7edd49d9a0da6f0
Aaron Gonzalez <aarongonzalez@ufl.edu>
2020-04-13 03:30:45 -0400

    Just a 3am pog farmer, farming Pogs

--- server/authserver/www/apis/rivalries/rivalry.php
+++ server/authserver/www/apis/rivalries/rivalry.php
@@ -5,43 +5,35 @@
 
 
 
-  if(isset($_GET['limit']) && isset($_GET['filter'])){ //This if Checks to see if its is a get, and if it has the requires params in this scenario I am checking to see if the param limnit and filter are given
+  if(isset($_GET['limit']) && isset($_GET['filter'])){ //GET request it gets the list of rivalries
     $conn = DataManager::getInstance()->getConnection("cities");
     if(!$conn || $conn->connect_error) exit(json_encode(array('error' => 'Could not connect to database. :(')));
-    // Store $_GET Params in variables
-    $id1 = $_GET['city_id1'];
-    $id2 = $_GET['city_id2'];
-    // SQL Statement here
-    $statement = $conn->prepare("SELECT * FROM rivalry WHERE name LIKE  CONCAT('%',?,'%') ORDER BY name ASC LIMIT ?");
+    $statement = $conn->prepare("SELECT Rivalryid, city_id1, name1,state1, country1, city_id2,name2,state2, country2 FROM
+(SELECT name1,state1, country1, city_id1,city_id2,rivalry.id as Rivalryid
+FROM rivalry
+JOIN (Select name as name1,state as state1, country as country1, id  FROM cities ) as Table2
+ON Table2.id=rivalry.city_id1) as Table3
+JOIN (Select name as name2,state as state2, country as country2 ,id FROM cities ) as Table4
+ON Table4.id=Table3.city_id2 WHERE name1 LIKE  CONCAT('%',?,'%') OR name2 LIKE  CONCAT('%',?,'%') LIMIT ?;");
     if(!$statement) error();
-    if(!$statement->bind_param("ss", $id1,  $id2)) error();
+    if(!$statement->bind_param("ssi", $_GET['filter'], $_GET['filter'],$_GET['limit'])) error();
     if(!$statement->execute()) exit(json_encode(array('error' => 'Query failed. :(')));
-    //Should there be more error checking here?
     $resultSet = $statement->get_result();
     $output = array();
-    while($row = $resultSet->fetch_assoc()){ // loop through all the results
-      array_push($output, $row);//push the information into the array , side note $row['Column name'] will get you specific info from that column of the row
+    while($row = $resultSet->fetch_assoc()){
+      array_push($output, $row);
     }
-    // get the result set and put it in an array and return the Json encoded object
     exit(json_encode($output));
   }
-  else if(isset($_POST['id']) &&(isset($_POST['delete']) && $_POST['delete']) ){
-    // Store $_POST Params in variables
-    // SQL Statement here ("SELECT * FROM rivalry WHERE id = ?")
-    // get the result set and put it in an array and return the Json encoded object
+  else if(isset($_POST['id']) &&(isset($_POST['delete']) && $_POST['delete']) ){ // POST Request to delete Rivalries
     $conn = DataManager::getInstance()->getConnection("cities");
     if(!$conn || $conn->connect_error) exit(json_encode(array('error' => 'Could not connect to database. :(')));
     $statement = $conn->prepare("DELETE FROM rivalry WHERE id = ?");
     if(!$statement) error();
     if(!$statement->bind_param("i", intval($_POST["id"]))) error();
     if(!$statement->execute()) exit(json_encode(array('error' => 'Query failed. :(')));
-    //Should there be more error checking here?
-    $resultSet = $statement->get_result();
   }
-  else if(isset($_POST['city_id1']) && isset($_POST['city_id2'])){
-    // Store $_POST Params in variables
-    // SQL Statement here ("INSERT INTO rivalries (city_id , city_id2) VALUES ( ? , ?)")
-    // get the result set and put it in an array and return the Json encoded object
+  else if(isset($_POST['city_id1']) && isset($_POST['city_id2'])){ //POST REQUEST TO SAVE RIVALRIES
     $conn = DataManager::getInstance()->getConnection("cities");
     if(!$conn || $conn->connect_error) exit(json_encode(array('error' => 'Could not connect to database. :(')));
     $id1 = $_POST['city_id1'];
@@ -56,8 +48,6 @@
     if(!$statement) error();
     if(!$statement->bind_param("ss", $id1,  $id2)) error();
     if(!$statement->execute()) exit(json_encode(array('error' => 'Query failed. :(')));
-    //Should there be more error checking here?
-    $resultSet = $statement->get_result();
 
   }
   else{
